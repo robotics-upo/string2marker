f2c286793a9038b92314bd790df697302f1b3dc2
RicardoRagel <ricardoragel4@gmail.com>
2022-01-27 17:58:52 +0100

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}Installing ROS dependencies ...${NC}"
-sudo apt-get install ros-$ROS_DISTRO-rosbag ros-$ROS_DISTRO-image-view ros-$ROS_DISTRO-image-transport ros-$ROS_DISTRO-image-view
+sudo apt-get install ros-$ROS_DISTRO-std-msgs ros-$ROS_DISTRO-visualization-msgs
